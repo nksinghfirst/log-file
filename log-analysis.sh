d453914e2018466ad688a8eaa7373e9c0f693723
nksinghfirst <117273440+nksinghfirst@users.noreply.github.com>
2024-04-23 21:22:22 +0530

    Update log-analysis.sh

--- log-analysis.sh
+++ log-analysis.sh
@@ -6,7 +6,7 @@ search_keyword()
   read -p "Enter a keyword that you want to search for: " keyword
   if grep -q "$keyword" "$file_name"
    then   
-     grep -n -i "$keyword" "$file_name"
+     grep -n -i "$keyword" "$file_name" | tail -f $file_name 
    else
      echo "No match found for the keyword"
   fi

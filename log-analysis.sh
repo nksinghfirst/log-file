f2dcd82df038c9b8409e927a2162dd63a0afdee0
nksinghfirst <117273440+nksinghfirst@users.noreply.github.com>
2024-04-23 18:53:13 +0530

    Create log-analysis.sh

---
+++ log-analysis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+search_keyword() 
+{
+  read -p "Enter file path or file name: " file_name
+  read -p "Enter a keyword that you want to search for: " keyword
+  if grep -q "$keyword" "$file_name"
+   then   
+     grep -n -i "$keyword" "$file_name"
+   else
+     echo "No match found for the keyword"
+  fi
+}
+
+search_keyword
